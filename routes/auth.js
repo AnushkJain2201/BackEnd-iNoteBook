bde2fd0b34f26b07d3585042503dd625559f374c
Anushk Jain <anushk2201@gmail.com>
2023-06-19 19:00:54 +0530

    bcryptjs jsonwebtoken done

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const  jwt = require('jsonwebtoken');
+const JWT_SECRET = "Harryisagoodb$oy";
+
 
 
 // Same As app.get In The index.js
@@ -32,14 +36,23 @@ router.post('/createuser', //The Following Array Contains The Validation Methods
                 return res.status(400).json({ error: "Sorry A User With this Email Already Exist" });
             }
 
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password , salt);
             // Else Do This
             // This Method Will Create All The Field As Per Our User Schema And Save It To Our Mongo DataBase
             user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPass,
                 email: req.body.email
-            })
-            res.json(user)
+            });
+
+            const data = {
+                user:{
+                    id: user.id
+                }
+            }
+            const authtoken = jwt.sign(data , JWT_SECRET);
+            res.json({authtoken});
         } catch (error) {
             console.log(error.message);
             res.status(500).send("Some Error Occurred");
