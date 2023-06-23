753dc2e2857de5a2321b34c59e05a290388a3650
Anushk Jain <anushk2201@gmail.com>
2023-06-23 21:20:15 +0530

    fix cors fetch problema

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import Statements
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors')
 
 // Call Function To Connect To MongoDB
 connectToMongo();
@@ -11,6 +12,9 @@ const port = 5000
 
 // We Have To Use A MiddleWare To Use The req.body
 app.use(express.json());
+
+
+app.use(cors())
 
 // Available Routes
 app.use('/api/auth' , require('./routes/auth'));
